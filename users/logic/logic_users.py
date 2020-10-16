ad5bb0fe239ae0900ee06af05a5cf4472e523de6
santiagocala <s.cala@uniandes.edu.co>
2020-10-15 19:05:58 -0500

    se subieron los cambios

---
+++ users/logic/logic_users.py
@@ -0,0 +1,5 @@
+from ..models import User
+
+def get_all_users():
+    users = User.objects.all()
+    return users
--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .logic.logic_users import get_all_users
+from django.http import HttpResponse
+from django.core import serializers
 
-# Create your views here.
+def get_users(request):
+    users = get_all_users()
+    users_list = serializers.serialize('json', users)
+    return HttpResponse(users_list, content_type = 'application/json')
